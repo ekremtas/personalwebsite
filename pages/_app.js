c2f3aa2eb3c64135cf1fab7ab3460321db29b0e6
ekremtas <ekremtas589@gmail.com>
2021-02-19 19:52:05 +0300

    PW-1 Eslint & prettier & husky libraries is added and configured this project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import React from 'react'
 import '../styles/globals.css'
+import PropTypes from 'prop-types'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType,
+  pageProps: PropTypes.object
 }
 
 export default MyApp
