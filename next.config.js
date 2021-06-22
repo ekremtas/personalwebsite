135e46c73c9bc7cef82fd3f8b3ac150efb904663
EKREM TAŞ <ekremtas@EKREMs-MacBook-Air.local>
2021-06-22 14:09:12 +0300

    PW-4 imgix added

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = {
     localeSubpaths
   },
   images: {
+    loader: 'imgix',
     domains: ['images.unsplash.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840]
   }
